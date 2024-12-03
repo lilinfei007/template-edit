88a90b450103ffad321d876da7eaedddc9ff20fc
DESKTOP-0KEUAJH\qllsd <1318151255@qq.com>
2024-12-03 17:47:11 +0800

    add

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,47 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <el-button size="small" type="primary" icon="el-icon-plus" @click="pushData"></el-button>
+    <el-tree ref="tree" node-key="id" :default-checked-keys="checkedKeys" show-checkbox :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
   </div>
 </template>
+<script>
+export default {
+  name:"AbouteView",
+  data(){
+    return {
+      checkedKeys:[2],
+      data:[{
+        label:"1级 1",
+        id:1,
+        children:[{
+          id:2,
+          label:"2级 1-1"
+        },{
+          id:3,
+          label:'2级 1-2'
+        }]
+      }],
+      defaultProps:{
+        children:'children',
+        label:"label"
+      }
+    }
+  },
+  methods:{
+    pushData(){
+      this.$refs.tree.append({
+        label:`2级 1-${this.data[0].children.length+1}`
+      },1)
+    },
+    handleNodeClick(data){
+      console.log(data);
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+.about{
+  padding:20px;
+  border:1px solid #999;
+}
+</style>
--- src/views/ProjectEdit.vue
+++ src/views/ProjectEdit.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    {{ curProject }}
+
   </div>
 </template>
 <script>
@@ -9,13 +9,12 @@ export default {
   name:"ProjectEdit",
   data(){
     return {
-      curProject:{},
-      projectList:localStorage("projectList")
+
+
     }
   },
   created(){
-    this.curProject = this.projectList.find(item => item.id == this.$route.query.id) || {};
-    if(Object.values(this.curProject).length === 0) this.$message.error("获取项目配置信息失败")
+
   }
 }
 </script>
